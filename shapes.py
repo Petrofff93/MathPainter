0520d09488fa354fbc373e1d7e423e98a98126b5
Petrofff93 <97982407+Petrofff93@users.noreply.github.com>
2022-02-27 19:40:20 +0200

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,23 @@
+class Rectangle:
+    def __init__(self, x, y, height, width, color):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.color = color
+
+    def draw(self, canvas):
+        """Draws itself into the canvas"""
+        canvas.data[self.x: self.x + self.height, self.y: self.y + self.width] = self.color
+
+
+class Square:
+    def __init__(self, x, y, side, color):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.side = side
+
+    def draw(self, canvas):
+        """Draws itself into the canvas"""
+        canvas.data[self.x: self.x + self.side, self.y: self.y + self.side] = self.color
